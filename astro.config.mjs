79d0ef51bc61f5bffb971c0c27c68488eb47735b
ddavejjackson <protoblaster11@outlook.ie>
2026-08-08 19:33:22 +0100

    9th commit - Update astro.config.mjs - remove unused code

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
-import vercel from '@astrojs/vercel';
-import decapCmsOauth from 'astro-decap-cms-oauth';
 
 export default defineConfig({
-  output: 'static',
-  adapter: vercel(),
-  integrations: [decapCmsOauth],
   vite: {
     plugins: [tailwindcss()],
   },
